9051c083ba9c55872ec674f32bc764eaa12a14fd
Adam Flett <adam@adamflett.net>
2023-11-04 19:50:04 +0800

    basic setup

---
+++ editor/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Starting editor."
+              << "\n";
+    return 0;
+}
---
+++ game/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Starting game."
+              << "\n";
+    return 0;
+}
